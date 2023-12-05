3957393debf12ccdedec74281e085604dd801147
kevin acosta <kevinacost808@gmail.com>
2023-12-04 23:52:32 -0500

    controller

--- src/main/java/com/kevin/student/controller/StudentController.java
+++ src/main/java/com/kevin/student/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.kevin.student.controller;
 
+
+import com.kevin.student.models.Student;
 import com.kevin.student.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/student")
@@ -13,5 +17,43 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
-    
+    @GetMapping
+    public String studentList(Model model){
+        List<Student> studentsList = studentService.getAll();
+        model.addAttribute("studentsList",studentsList);
+        return "index";
+    }
+
+    @GetMapping("/openForm")
+    public String openForm(Model model){
+        model.addAttribute("student", new Student());
+        model.addAttribute("action", "/student/newStudent");
+        return "form";
+    }
+
+    @PostMapping("/newStudent")
+    public String addStudent(@ModelAttribute Student student){
+        studentService.addStudent(student);
+        return "redirect:/student";
+    }
+
+    @GetMapping("/openFormEdit/{id}")
+    public String openFormEdit(@PathVariable Long id, @ModelAttribute Student student, Model model){
+        model.addAttribute("student", student);
+        model.addAttribute("action", "/student/editStudent/"+id);
+        return "editForm";
+    }
+
+    @PostMapping("/editStudent/{id}")
+    public String editStudent(@PathVariable Long id, @ModelAttribute Student student){
+        studentService.editStudent(id, student);
+        return "redirect:/student";
+    }
+
+    @GetMapping("/deleteStudent/{id}")
+    public String deleteStudent(@PathVariable Long id){
+        studentService.deleteStudent(id);
+        return "redirect:/student";
+    }
+
 }
